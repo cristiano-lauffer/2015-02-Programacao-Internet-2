00d29d37b36d879e2b4c09eb3ebf1de4ec9a7b56
Cristiano Lauffer <cristiano.serafim.lauffer@gmail.com>
2015-11-17 02:06:05 -0200

    Melhorias JPA - Inclusão de script para a execução pós-implantação da aplicação JSF. - Este script deve ser executado após a criação das tabelas através da aplicação JSF.

---
+++ database/04.Insert.Pos.JPA.sql
@@ -0,0 +1,29 @@
+﻿--SELECT * FROM cargo
+
+insert into cargo (
+	--id,
+	nome
+) values (
+	--1,
+	'Analista BI'
+);
+
+--select * from usuario
+
+insert into usuario (
+	--id,
+	administrador,
+	cpf,
+	nome,
+	senha,
+	usuariosistema,
+	cargo_id
+) values (
+	--1,
+	'T',
+	'01040337031',
+	'Cristiano',
+	'123',
+	'clauffer',
+	1
+);
